e9a3200a4e3d3b77fe8a092bf8314fc66c0f0efc
Milan <milan@mucka.sk>
2019-09-05 15:50:58 +0300

    Add addCosts functionality

--- app/src/main/java/com/example/costs/AddActivity.java
+++ app/src/main/java/com/example/costs/AddActivity.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -25,11 +27,31 @@ public class AddActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+            public void onClick(View view) { addNewCosts(view); }
         });
+
+        EditText addDateEdt = findViewById(R.id.editTextAddDate);
+//        addDateEdt.setAutofillId();
+        ImageButton imgBtn = findViewById(R.id.imgBtnRecipe);
+        //imgBtn.setOnClickListener(nnew View.on);
+
+    }
+
+    private void addNewCosts(View view){
+        EditText addNameEdt = findViewById(R.id.editTextAddName);
+        EditText addDateEdt = findViewById(R.id.editTextAddDate);
+        EditText addDescEdt = findViewById(R.id.editTextAddDescription);
+        EditText addPriceEdt = findViewById(R.id.editTextAddPrice);
+
+        String name = addNameEdt.getText().toString();
+
+        String date = addDateEdt.getText().toString();
+        String desc = addDescEdt.getText().toString();
+        String price = addPriceEdt.getText().toString();
+
+        Snackbar.make(view, name+date+desc+price, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
     }
 
 }
