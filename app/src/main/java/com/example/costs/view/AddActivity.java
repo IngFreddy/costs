abd7a82893054f6392803e8d6dfee206927f2be1
Milan <milan.freddy@gmail.com>
2019-09-13 16:46:06 +0200

    Editing of existing cost, category

--- app/src/main/java/com/example/costs/view/AddActivity.java
+++ app/src/main/java/com/example/costs/view/AddActivity.java
@@ -8,6 +8,7 @@ import com.example.costs.database.CategoryDB;
 import com.example.costs.database.CostDB;
 import com.example.costs.database.DatabaseHelper;
 import com.example.costs.database.model.Category;
+import com.example.costs.database.model.Cost;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,141 +16,189 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddActivity extends AppCompatActivity {
     private List<Category> categories = new ArrayList<>();
+    private String date;
+    private int _id;
+    private int update = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        // ENABLE TOOLBAR
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        Intent intent = getIntent();
-        String date = intent.getStringExtra("date");
-        setResult(50, intent);
+        Button save=new Button(this);       //SAVE BUTTON TOOLBAR
+        save.setBackground(getDrawable(R.drawable.ic_done_button_white));
+        Toolbar.LayoutParams tl=new Toolbar.LayoutParams(Toolbar.LayoutParams.WRAP_CONTENT, Toolbar.LayoutParams.WRAP_CONTENT);
+        tl.gravity= Gravity.END;
+        save.setLayoutParams(tl);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { addNewCosts(view); }
+        });
+        toolbar.addView(save);
 
+        // SAVE BUTTON
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { addNewCosts(view); }
         });
 
-        EditText addDateEdt = findViewById(R.id.editTextAddDate);
-        addDateEdt.setText(date);
 
         ImageButton imgBtn = findViewById(R.id.imgBtnRecipe);
-        //imgBtn.setOnClickListener(nnew View.on);
+        //imgBtn.setOnClickListener(new View.on);
 
-        getCategories();
+        getCategories();        // FILL CATEGORIES SPINNER FROM DB
 
-    }
-
-    private void getCategories(){
-        CategoryDB db = new CategoryDB(this);
-        categories = db.getAllCategories();
-
-        Spinner addCategSpin = findViewById(R.id.spinnerChooseCategorie);
-
-        List<String> spinnerArray =  new ArrayList<String>();
-        for(Category cat: categories){
-            spinnerArray.add(cat.getName());
+        // GET DATA FROM PARENT ACTIVITY
+        Intent intent = getIntent();
+        date = intent.getStringExtra("date");
+        update = intent.getIntExtra("update", 0);
+        if(update == 1){
+            updatingCost(intent.getIntExtra("id", 0));
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, spinnerArray);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        addCategSpin.setAdapter(adapter);
+        EditText addDateEdt = findViewById(R.id.editTextAddDate);
+        addDateEdt.setText(date);
     }
 
 
-    private void addNewCosts(View view){
-        EditText addNameEdt = findViewById(R.id.editTextAddName);
-        EditText addDateEdt = findViewById(R.id.editTextAddDate);
-        EditText addDescEdt = findViewById(R.id.editTextAddDescription);
-        EditText addPriceEdt = findViewById(R.id.editTextAddPrice);
-        Spinner addCategSpin = findViewById(R.id.spinnerChooseCategorie);
+    /**
+     * UPDATING cost in database
+     * prefilll informations into formullar
+     * @param costId    ID of editing item
+     */
+    private void updatingCost(int costId){
+        final EditText addNameEdt = findViewById(R.id.editTextAddName);
+        final EditText addDescEdt = findViewById(R.id.editTextAddDescription);
+        final EditText addPriceEdt = findViewById(R.id.editTextAddPrice);
+        final Spinner addCategSpin = findViewById(R.id.spinnerChooseCategorie);
 
-        String name = addNameEdt.getText().toString();
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        Cost cost;
+        try {
+            CostDB db = new CostDB(this);
+            cost = db.selectCost(costId);
+
+            addNameEdt.setText(cost.getName());
+            addDescEdt.setText(cost.getDescription());
+            addPriceEdt.setText(decimalFormat.format(cost.getPrice()));
+            int i=0;
+            for(; i<categories.size(); i++){
+                if(categories.get(i).getId() == cost.getCategoryID())  break;
+            }
+            addCategSpin.setSelection(i);
+
+            this._id = costId;
+        }
+        catch (NoSuchFieldException e){
+            Snackbar.make(this.getCurrentFocus(), "Error reading cost DB: "+ e.getMessage(), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    }
+
+    /**
+     * READ from DB and FILL categories into Spinner
+     */
+    private void getCategories(){
+        try {
+            CategoryDB db = new CategoryDB(this);
+            categories = db.getAllCategories();
+
+            Spinner addCategSpin = findViewById(R.id.spinnerChooseCategorie);
+
+            List<String> spinnerArray = new ArrayList<String>();
+            for (Category cat : categories) {
+                spinnerArray.add(cat.getName());
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this, android.R.layout.simple_spinner_item, spinnerArray);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            addCategSpin.setAdapter(adapter);
+        }
+        catch (Exception e){
+            Snackbar.make(this.getCurrentFocus(), "ERROR reading Categories: "+ e.getMessage(), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    }
+
+    /**
+     * SAVE cost into DB with filled informations
+     * - name, description, price, date, category
+     * @param view
+     */
+    private void addNewCosts(View view){
+        final EditText addNameEdt = findViewById(R.id.editTextAddName);
+        final EditText addDescEdt = findViewById(R.id.editTextAddDescription);
+        final EditText addPriceEdt = findViewById(R.id.editTextAddPrice);
+        final EditText addDateEdt = findViewById(R.id.editTextAddDate);
+        final Spinner addCategSpin = findViewById(R.id.spinnerChooseCategorie);
+
+        String name = addNameEdt.getText().toString();      //read input
         float price = 0f;
         String date = addDateEdt.getText().toString();
         String desc = addDescEdt.getText().toString();
-        int cats = categories.get(addCategSpin.getSelectedItemPosition()).getId();
+        int cat = categories.get(addCategSpin.getSelectedItemPosition()).getId();  //category ID
 
         try {
-            price = Float.valueOf(addPriceEdt.getText().toString());
+            price = Float.valueOf(addPriceEdt.getText().toString());        //parse price into float
         }
         catch (Exception e){
             Snackbar.make(view, "NOT VALID PRICE: "+addPriceEdt.getText(), Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
-
-            Log.d("FLOAT",e.getMessage());
-
             return;
         }
 
-        Snackbar.make(view, "SAVED:" + name+desc+price+date, Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
-
         try {
             CostDB db = new CostDB(this);
-            db.insertCost(name, desc, price, date, cats);
+            if(update == 1){    // UPDATE
+                db.updateCost(_id, name, desc, price, date, cat);
+            }
+            else {              // ADD NEW
+                db.insertCost(name, desc, price, date, cat);
+            }
         }
         catch (Exception e){
-            Snackbar.make(view, "ERROR writing in database:"+e.getMessage(), Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "ERROR writing in database: "+e.getMessage(), Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
             return;
         }
 
-        Intent intent = getIntent();
-        setResult(RESULT_OK,intent);
-
+        setResult(1);
         finish();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_costs, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        if (item.getItemId() == android.R.id.home) {
-            Intent intent = getIntent();
-            setResult(RESULT_CANCELED, intent);
-
+        if (item.getItemId() == android.R.id.home) {        //GO BACK
+            setResult(RESULT_CANCELED);
             finish();
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-
 }
